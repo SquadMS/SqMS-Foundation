9c011b3ac70e6d21c8dff460b090c5fadccb9a6d
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-18 07:29:13 +0200

    Update model,

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,6 +6,27 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-  // Disable Laravel's mass assignment protection
-  protected $guarded = [];
+    /* Disable Laravel's mass assignment protection */
+    protected $guarded = [
+        //
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'api_token',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'last_fetched' => 'datetime',
+    ];
 }
