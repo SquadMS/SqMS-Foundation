df1ab498388ea53b1236ab4083ce5b976e032695
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-18 06:56:19 +0200

    Add steam_id_64 parameter to route,

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 /* User Profile */
-Route::get(Config::get('sqms.user.route.url'), [Config::get('sqms.user.route.controller'), 'show'])->name(Config::get('sqms.user.route.name'));
+Route::get(Config::get('sqms.user.route.url') . '/{steam_id_64}', [Config::get('sqms.user.route.controller'), 'show'])->name(Config::get('sqms.user.route.name'));
 
 /* SteamLogin */
 Route::get('steam/login', [Config::get('sqms.auth.controller'), 'login'])->name('steam.login');
