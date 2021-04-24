efea8e1d7329554ce7f4b8ea42e0d91276334ded
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-24 07:43:38 +0200

    Fix unknown property user on CheckAdminAreaAccess Middleware,

--- src/Admin/Http/Middleware/CheckAdminAreaAccess.php
+++ src/Admin/Http/Middleware/CheckAdminAreaAccess.php
@@ -9,7 +9,7 @@ class CheckAdminAreaAccess
 {
     public function handle($request, Closure $next)
     {
-        if ($this->user && ($this->user->isSystemAdmin() || $this->user->can('admin'))) {
+        if ($request->user && ($request->user->isSystemAdmin() || $request->user->can('admin'))) {
             return $next($request);
         } else {
             return redirect()->route(Config::get('sqms.routes.def.home.name'));
