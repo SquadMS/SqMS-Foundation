c1e3be9986dbb4e2955168d7b3b7d633d01c8b51
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-24 07:41:23 +0200

    Restructure SquadMSFoundationServiceProvider, Load Middleware,

--- src/SquadMSFoundationServiceProvider.php
+++ src/SquadMSFoundationServiceProvider.php
@@ -27,12 +27,17 @@ class SquadMSFoundationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /* Configuration */
         $this->mergeConfigFrom(__DIR__ . '/../config/sqms.php', 'sqms');
 
+        /* Migrations */
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        /* Middlewares */
+        $router = $this->app->make(Router::class);
+        $router->aliasMiddleware('checkAdminAreaAccess', \SquadMS\Foundation\Admin\Http\Middleware\CheckAdminAreaAccess::class);
 
+        /* Routes */
         Route::group([
             'prefix' => config('sqms.routes.prefix'),
             'middleware' => config('sqms.routes.middleware'),
