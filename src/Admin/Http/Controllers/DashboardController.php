3a3f4ced9f23331c0884cb3d6eef7b57b4c5e0d1
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-24 07:55:21 +0200

    Remove unused copy n paste code from DashboardController

--- src/Admin/Http/Controllers/DashboardController.php
+++ src/Admin/Http/Controllers/DashboardController.php
@@ -4,7 +4,6 @@ namespace SquadMS\Foundation\Admin\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Config;
-use SquadMS\Foundation\Repositories\UserRepository;
 
 class DashboardController extends Controller
 {
@@ -13,11 +12,8 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show(string $steamId64)
+    public function show()
     {
-        /** @var \App\Models\User Find user given steamId64 */
-        $user = UserRepository::getUserModelQuery()->where('steam_id_64', $steamId64)->firstOrFail();
-
         /* Show profile page */
         return view(Config::get('sqms.theme') . '::admin.pages.dashboard');
     }
