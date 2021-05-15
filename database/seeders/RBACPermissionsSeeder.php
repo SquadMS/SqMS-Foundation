ceeaece235daad1c2f1db6a78e861a23a9e0804a
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-15 02:13:16 +0200

    Fix RBACPermissionsSeeder,

--- database/seeders/RBACPermissionsSeeder.php
+++ database/seeders/RBACPermissionsSeeder.php
@@ -16,9 +16,12 @@ class RBACPermissionsSeeder extends Seeder
      */
     public function run()
     {
+        /* Get currently registered Permission definitions */
+        $definitions = SquadMSPermissions::getPermissions();
+
         /* Properly format the permission definitions array and prefix it with the name key for upserting */
         $pairs = [];
-        foreach (array_keys(SquadMSPermissions::getPermissions()) as $definition) {
+        foreach (array_keys($definitions) as $definition) {
             $pairs[] = [
                 'name' => $definition,
                 'guard_name' => Config::get('auth.defaults.guard', 'web'),
@@ -27,5 +30,8 @@ class RBACPermissionsSeeder extends Seeder
 
         /* Mass Update/Insert the Permissions */
         Permission::query()->upsert($pairs, ['name'], ['name']);
+
+        /* Remove obsolete Permissions */
+        Permission::whereNotIn('name', array_keys($definitions))->delete();
     }
 }
