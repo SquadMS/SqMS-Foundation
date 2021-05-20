37b7d68cd721cf7e6cae91713a2474eb4aaa93a5
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-20 02:33:13 +0200

    Fix admin footer,

--- resources/views/admin/structure/footer.blade.php
+++ resources/views/admin/structure/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 text-center col-md-6 text-md-left">
-                © {{ now()->year }} {{ config('app.name') }}. All rights reserved!{{ __('squadms-default-theme::footer.copyright', ['name' => config('app.name'), 'year' => now()->year]) }}
+                © {{ now()->year }} {{ config('app.name') }}. All rights reserved!
             </div>
             <div class="col-12 text-center col-md-6 text-md-right">
                 Proudly powered by <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>
