8137249320c0393e3813487fc9ba84095d2dcae1
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-23 01:24:20 +0200

    Add new admin menus to admin template,

--- resources/views/admin/structure/layout.blade.php
+++ resources/views/admin/structure/layout.blade.php
@@ -44,9 +44,27 @@
             <div class="row">
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
                     <div class="position-sticky pt-3">
+                        @php
+                            $mainMenu = \SquadMSMenu::getMenu('admin-system')->setWrapperTag(); 
+                        @endphp
+                        @if ($mainMenu->count())
                         <ul class="nav flex-column">
                             {!! \SquadMSMenu::getMenu('admin')->setWrapperTag()->render() !!}
                         </ul>
+                        @endif
+
+                        @php
+                            $systemMenu = \SquadMSMenu::getMenu('admin-system')->setWrapperTag(); 
+                        @endphp
+                        @if ($systemMenu->count())
+                        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>System</span>
+                        </h6>
+
+                        <ul class="nav flex-column">
+                            {!! \SquadMSMenu::getMenu('admin-system')->setWrapperTag()->render() !!}
+                        </ul>
+                        @endif
                     </div>
                 </nav>
 
