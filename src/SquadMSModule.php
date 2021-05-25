1078c83ce8dbcd60ff7fb4ba972ee701fcca402c
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-25 03:08:13 +0200

    Fix assets publish command,

--- src/SquadMSModule.php
+++ src/SquadMSModule.php
@@ -20,7 +20,7 @@ class SquadMSModule extends SquadMSModuleContract {
     {
         Artisan::call('vendor:publish', [
             '--provider' => SquadMSFoundationServiceProvider::class,
-            '--tags'     => 'assets',
+            '--tag'     => 'assets',
         ]);
     }
 }
