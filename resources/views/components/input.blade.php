4cc18d82e5c22a33dde06f616e075daff74e49a7
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-30 06:03:16 +0200

    Add input component

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,24 @@
+@props(['id', 'name', 'disabled' => false, 'type' => 'text', 'placeholder' => '', 'label' => false, 'help' => false])
+
+@php
+    $id = $id ?? \Illuminate\Support\Str::random();
+@endphp
+
+<div {{ $attributes }}>
+    @if ($label)
+        <label for="{{ $id }}" class="form-label">{{ $label }}/label>
+    @endif
+    <input id="{{ $id }}" class="form-control {{ $name && $errors->has($name) ? 'is-invalid' : '' }}" name="{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}" aria-label="{{ $label ?? $placeholder }}" {{ $disabled ? 'disabled' : ''}}>
+
+    @if ($help)
+        <div id="{{ $id }}Help" class="form-text">{{ $help }}</div>
+    @endif
+
+    @if ($name)
+        @error($name) 
+            <div id="validation{{ $id }}" class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    @endif
+</div>
