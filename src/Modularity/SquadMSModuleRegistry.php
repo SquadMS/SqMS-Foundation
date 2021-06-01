2a09d8843e5651702ed7773b3715041a4bdc3bb6
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-01 04:11:38 +0200

    Prevent modules registering the same menu multiple times,

--- src/Modularity/SquadMSModuleRegistry.php
+++ src/Modularity/SquadMSModuleRegistry.php
@@ -11,9 +11,12 @@ class SquadMSModuleRegistry
 {
     private Collection $store;
 
+    private Collection $registeredMenus;
+
     function __construct()
     {
         $this->store = new Collection();
+        $this->registeredMenus = new Collection();
     }
 
     public function register(string $module) : void
@@ -26,7 +29,7 @@ class SquadMSModuleRegistry
             throw new DuplicateModuleException('Modules can not be registeted twice!');
         }
 
-        /* Register the Modules admin menu orders. TODO: Move away */
+        /* Register the Modules admin menu orders */
         $module::registerAdminMenus();
 
         $this->store->put($module::getIdentifier(), $module);
@@ -42,9 +45,12 @@ class SquadMSModuleRegistry
 
     public function registerMenuEntries(string $menu) : void
     {
-        /** @var SquadMSModule $module */
-        foreach ($this->store as $identifier => $module) {
-            $module::registerMenuEntries($menu);
+        /* Do not register the same menu twice */
+        if (!$this->registeredMenus->has($menu)) {
+            /** @var SquadMSModule $module */
+            foreach ($this->store as $identifier => $module) {
+                $module::registerMenuEntries($menu);
+            }
         }
     }
 }
