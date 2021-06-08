e216c8ecddc84ca285d5b67b4edb51f42d81c60c
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-08 18:18:57 +0200

    Add default webRoutes helper,

--- src/Router/SquadMSRouter.php
+++ src/Router/SquadMSRouter.php
@@ -3,6 +3,8 @@
 namespace SquadMS\Foundation\Router;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
 use SquadMS\Foundation\Router\Exceptions\DuplicateRouteDefinitionException;
 
 class SquadMSRouter {
@@ -26,6 +28,27 @@ class SquadMSRouter {
     {
         foreach ($this->registry as $definition) {
             $definition();
+        }
+    }
+
+    static function webRoutes(array $definitions) : void
+    {
+        /* Define routes from config */
+        foreach ($definitions as $definition) {
+            /* Create the definitor as an anonymous function */
+            $define = function() use ($definition) {
+                $type = Arr::get($definition, 'type', 'get');
+
+                Route::$type(Arr::get($definition, 'path', '/'), [Arr::get($definition, 'controller'), Arr::get($definition, 'executor', 'show')])->middleware(Arr::get($definition, 'middlewares'))->name(Arr::get($definition, 'name'));
+            };
+
+            if (Arr::get($definition, 'localized', false)) {
+                Route::localized(function() use ($define) {
+                    $define();
+                });
+            } else {
+                $define();
+            }
         }
     }
 }
