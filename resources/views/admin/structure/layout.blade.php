bcdb381ef2d52ba822d0d91ab2c8aa5f49b10f10
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-11 20:58:27 +0200

    Add CSRF meta tag to admin layout,

--- resources/views/admin/structure/layout.blade.php
+++ resources/views/admin/structure/layout.blade.php
@@ -10,6 +10,9 @@
             @wat
         @endauth
 
+        <!-- CSRF -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
