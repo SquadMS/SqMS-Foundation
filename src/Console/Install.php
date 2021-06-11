7b641d6f25c67ccd2ca81c6a077b48beb14c7904
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-11 15:08:37 +0200

    Add replaceInFile Helper

--- src/Console/Install.php
+++ src/Console/Install.php
@@ -24,4 +24,17 @@ class Install extends Command
 
         $this->info('Setup complete!');
     }
+
+    /**
+     * Replace a given string within a given file.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $path
+     * @return void
+     */
+    protected function replaceInFile($search, $replace, $path)
+    {
+        file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
+    }
 }
