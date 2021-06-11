d463c9b331bd19658287090169f572ada05dd769
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-11 22:57:33 +0200

    Test

--- src/Auth/Http/Controllers/SteamLoginController.php
+++ src/Auth/Http/Controllers/SteamLoginController.php
@@ -4,6 +4,7 @@ namespace SquadMS\Foundation\Auth\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 use SquadMS\Foundation\Auth\SteamUser;
 use SquadMS\Foundation\Repositories\UserRepository;
@@ -21,10 +22,13 @@ class SteamLoginController extends AbstractSteamLoginController
         /* Login the user using the Auth facade */
         Auth::login($user, true);
 
+        /* Make sure the session is being saved after login so we have an id to reference */
+        Session::save();
+
         /* Create a new Websocket Token for the Session */
         $user->websocketTokens()->create([
             'token'      => Str::random(128),
-            'session_id' => $request->session()->getId(),
+            'session_id' => Session::getId(),
         ]);
     }
 }
