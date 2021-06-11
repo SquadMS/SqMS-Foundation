442568533c3b3e9b4096e8f54fc98364c1da01cb
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-11 21:31:58 +0200

    Add id column to websocket_tokens

--- database/migrations/2021_06_11_201750_create_websocket_tokens_table.php
+++ database/migrations/2021_06_11_201750_create_websocket_tokens_table.php
@@ -14,6 +14,8 @@ class CreateWebsocketTokensTable extends Migration
     public function up()
     {
         Schema::create('websocket_tokens', function (Blueprint $table) {
+            $table->id();
+            
             /* The Session related to this token  */
             $table->string('session_id')->references('id')->on('sessions')->onDelete('cascade');
 
