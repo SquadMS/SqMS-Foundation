83ae572d20181ef9504612e3fbec06102bd765e0
Patrick <privat@skyraptor.eu>
2022-06-13 22:22:15 +0200

    Update item.blade.php

--- resources/views/components/navigation/item.blade.php
+++ resources/views/components/navigation/item.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false, 'disabled' => false, 'link' => '#', 'onClick' => false, 'title'])
 
 <li {{ isset($attributes) ? $attributes->except(['slot'])->merge(['class' => 'nav-item']) : '' }}>
-    <a class="inline-block py-2 px-4 no-underline {{ $opacity-75 ? ' opacity-75' : '' }} {{ $active ? ' active' : '' }}" href="{{ is_callable($link) ? $link() : $link }}" {{ $onClick ? 'onClick="' . $onClick . '"' : ''}}>{!! $title !!}</a>
+    <a class="inline-block py-2 px-4 no-underline {{ $active ? ' active' : '' }}" href="{{ is_callable($link) ? $link() : $link }}" {{ $onClick ? 'onClick="' . $onClick . '"' : ''}}>{!! $title !!}</a>
     {!! $slot ?? '' !!}
 </li>
