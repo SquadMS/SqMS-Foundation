f1882aab30807c1b1171e86f7a7c021a5d619fd5
Patrick <privat@skyraptor.eu>
2022-06-13 02:37:56 +0200

    Create Translatable.php

---
+++ src/Resources/Concerns/Translatable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SquadMS\Foundation\Resources\Concerns\Translatable;
+
+trait Translatable {
+  use Filament\Resources\Concerns\Translatable;
+  
+  public static function getTranslatableLocales(): array
+  {
+      return config('sqms.locales');
+  }
+}
