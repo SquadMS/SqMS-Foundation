455edbb1b31f707f02e2363e8688724b0747d92d
Patrick <privat@skyraptor.eu>
2022-06-13 20:18:22 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>{{ __('sqms-default-theme::pages/profile.heading', ['name' => $user->name . '('. $user->steam_id_64 . ')']) }}</h1>
+                <h1>{{ __('sqms-foundation::pages/profile.heading', ['name' => $user->name . '('. $user->steam_id_64 . ')']) }}</h1>
             </div>
         </div>
     </div>
