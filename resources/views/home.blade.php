25bd0acd4a59de8f78775e0d3731d6b510794ebf
Patrick <privat@skyraptor.eu>
2022-06-14 23:00:43 +0200

    Fix home template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,13 @@
 @extends('sqms-foundation::structure.layout', [
-    'mainClass' => 'justify-content-center'
+    'mainClass' => 'justify-center'
 ])
 
 @section('content')
 <section>
     <div class="container mx-auto sm:px-4">
-        <div class="flex flex-wrap ">
-            <div class="relative flex-grow max-w-full flex-1 px-4 text-center">
-                <h1>{{ __('sqms-foundation::pages/home.heading') }}</h1>
-                <img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="max-w-full h-auto">
-            </div>
+        <div class="flex flex-col items-center flex-wrap">
+            <h1>{{ __('sqms-foundation::pages/home.heading') }}</h1>
+            <img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="max-w-full h-auto">
         </div>
     </div>
 </section>
