8c8a818441dde590f8dee120999f61ce255abf7d
Patrick <privat@skyraptor.eu>
2022-06-15 23:20:02 +0200

    Use variable instead of hardcoded markup

--- resources/lang/de/footer.php
+++ resources/lang/de/footer.php
@@ -2,5 +2,5 @@
 
 return [
     'copyright' => '© :year :name. Alle Rechte vorbehalten!',
-    'powered-by' => 'Stolz angetrieben durch <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
+    'powered-by' => 'Stolz angetrieben durch :brand'
 ];
--- resources/lang/he/footer.php
+++ resources/lang/he/footer.php
@@ -2,5 +2,5 @@
 
 return [
     'copyright' => '© :year :name. כל הזכויות שמורות!',
-    'powered-by' => 'מופעל בגאווה על ידי <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
+    'powered-by' => 'מופעל בגאווה על ידי :brand'
 ];
