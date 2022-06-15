06183abb56fade8741f3133dd9dd2a8af31d675f
Patrick <privat@skyraptor.eu>
2022-06-15 18:48:58 +0200

    Fix page template

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -5,11 +5,11 @@
     <div class="container mx-auto px-4">
         <div class="flex flex-wrap ">
             <div class="relative flex-grow max-w-full flex-1">
-                <h1>{{ $title }}</h1>
+                <h1>@yield('title')</h1>
             </div>
         </div>
 
-        {{ $content }}
+        @yield('content')
     </div>
 </section>
 @endsection
