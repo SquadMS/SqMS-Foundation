ca8ce3da1d47c3f5d4119c088fa995da1a701f52
Patrick <privat@skyraptor.eu>
2022-06-15 23:22:26 +0200

    Fix footer powered by logo style

--- resources/views/structure/footer.blade.php
+++ resources/views/structure/footer.blade.php
@@ -5,7 +5,7 @@
                 {{ __('sqms-foundation::footer.copyright', ['name' => config('app.name'), 'year' => now()->year]) }}
             </div>
             <div class="w-full text-center md:w-1/2 pr-4 pl-4 md:text-right">
-                {!! __('sqms-foundation::footer.powered-by') !!}
+                {!! __('sqms-foundation::footer.powered-by', ['brand' => '<a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="w-auto inline-block" style="height:1em"></a>']) !!}
             </div>
         </div>
     </div>
