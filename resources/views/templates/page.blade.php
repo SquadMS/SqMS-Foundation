9fbff85d1eb65fe8b52a2fc99f2ab37b44fa5632
Patrick <privat@skyraptor.eu>
2022-06-26 22:01:00 +0200

    Preix tailwind on page template

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -1,17 +1,17 @@
 @extends('sqms-foundation::structure.layout')
 
 @section('content')
-<section class="bg-gray-200 py-5">
-    <div class="container mx-auto px-4">
-        <div class="flex flex-wrap">
-            <div class="relative flex-grow max-w-full flex-1">
+<section class="sqmsf-bg-gray-200 sqmsf-py-5">
+    <div class="sqmsf-container sqmsf-mx-auto sqmsf-px-4">
+        <div class="sqmsf-flex sqmsf-flex-wrap">
+            <div class="sqmsf-relative sqmsf-flex-grow sqmsf-max-w-full sqmsf-flex-1">
                 <h1 class="text-xl font-bold">@yield('title')</h1>
             </div>
         </div>
     </div>
 </section>
 <section>
-    <div class="container mx-auto px-4 py-5">
+    <div class="sqmsf-container sqmsf-mx-auto sqmsf-px-4 sqmsf-py-5">
         @yield('page-content')
     </div>
 </section>
