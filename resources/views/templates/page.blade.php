3faaac96352e22423300220600e029f5ff44f5ab
Patrick <privat@skyraptor.eu>
2022-06-26 16:36:51 +0200

    WIP: work on page template

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -1,14 +1,17 @@
 @extends('sqms-foundation::structure.layout')
 
 @section('content')
-<section class="mt-6">
+<section class="bg-slate-600 py-4">
     <div class="container mx-auto px-4">
         <div class="flex flex-wrap ">
             <div class="relative flex-grow max-w-full flex-1">
                 <h1>@yield('title')</h1>
             </div>
         </div>
-
+    </div>
+</section>
+<section>
+    <div class="container mx-auto px-4">
         @yield('page-content')
     </div>
 </section>
