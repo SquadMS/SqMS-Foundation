ce9537fd4fe9ff37ce2b837ab8c090a2024d6698
Patrick <privat@skyraptor.eu>
2022-06-28 20:53:50 +0200

    Make profile image square

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,7 +2,7 @@
     <div class="md:sqmsf-flex sqmsf-no-wrap">
         <!-- Info Frame -->
         <div class="sqmsf-w-full md:sqmsf-w-3/12 md:sqmsf-mr-2 sqmsf-p-4 sqmsf-bg-gray-200">
-            <img class="sqmsf-w-full sqmsf-mb-6" src="{{ $user->avatar }}" />
+            <img class="sqmsf-w-full sqmsf-mb-6 sqmsf-aspect-square" src="{{ $user->avatar }}" />
 
             <h2 class="sqmsf-font-bold sqmsf-text-xl sqmsf-whitespace-nowrap sqmsf-text-ellipsis sqmsf-overflow-hidden">{{ $user->name }}</h1>
             <p class="sqmsf-whitespace-nowrap sqmsf-text-ellipsis sqmsf-overflow-hidden">{{ $user->steam_id_64 }}</p>
