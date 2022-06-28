32efbb3716f657286702dd9c358721b9461ae8a9
Patrick <privat@skyraptor.eu>
2022-06-28 20:07:14 +0200

    Change: Add steam id to profile page info frame,

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,11 +1,15 @@
 <x-sqms-foundation::templates.page :title="__('sqms-foundation::pages/profile.heading', ['name' => $user->name . '('. $user->steam_id_64 . ')'])">
     <div class="md:sqmsf-flex sqmsf-no-wrap">
-        <div class="sqmsf-w-full md:sqmsf-w-3/12 md:sqmsf-mr-2 sqmsf-px-4 sqmsf-pt-4 sqmsf-bg-gray-200">
+        <!-- Info Frame -->
+        <div class="sqmsf-w-full md:sqmsf-w-3/12 md:sqmsf-mr-2 sqmsf-p-4 sqmsf-bg-gray-200">
             <img class="sqmsf-w-full sqmsf-mb-6" src="{{ $user->avatar }}" />
 
             <h1 class="sqmsf-font-bold sqmsf-text-xl sqmsf-whitespace-nowrap sqmsf-text-ellipsis sqmsf-overflow-hidden">{{ $user->name }}</h1>
+            <p>{{ $user->steam_id_64 }}</p>
         </div>
-        <div class="sqmsf-w-full md:sqmsf-w-9/12 md:sqmsf-ml-2 sqmsf-px-4 sqmsf-pt-4 sqmsf-bg-gray-200">
+
+        <!-- Content Frame -->
+        <div class="sqmsf-w-full md:sqmsf-w-9/12 md:sqmsf-ml-2 sqmsf-4 sqmsf-bg-gray-200">
             <h2>About</h2>
         </div>
     </div>
