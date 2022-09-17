8f90cfb5927f0958f85cc41fe33d4571654777d7
Patrick <privat@skyraptor.eu>
2022-09-17 20:11:05 +0200

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,5 @@
-<a href="{{ route('steam.login') }}">Login</a>
+<div class="space-y-8">
+  <x-filament::button tag="a" href="{{ route('steam.login') }}" class="w-full">
+      {{ __('filament::login.buttons.submit.label') }}
+  </x-filament::button>
+</div>
